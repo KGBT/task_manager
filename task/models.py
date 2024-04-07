be5bcb495828d1c37978f8773fbab4aae8ff0146
KGBT <nik.ivan98@mail.ru>
2024-04-07 20:30:10 +0500

    Создал модели для модулей task и user.

--- task/models.py
+++ task/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from user.models import User
+
+
 # Create your models here.
+
+class Task(models.Model):
+    users = models.ManyToManyField(User, through='UserTask')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000, null=True)
+    date_start = models.DateField()
+    date_end = models.DateField()
+
+
+class UserTask(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    status = models.CharField(max_length=15)
+    message = models.TextField(max_length=1000, null=True)
+
+
+class Priority(models.Model):
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, primary_key=True)
+    priority = models.CharField(max_length=15)
+
+
+class File(models.Model):
+    task = models.ForeignKey(to=Task, on_delete=models.CASCADE, null=True)
+    file = models.FileField(upload_to='files/', null=True)
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#Папка для сохранения файлов
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task',
+    'user',
 ]
 
 MIDDLEWARE = [

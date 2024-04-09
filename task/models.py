2b8dc42ddf3f1c21431bc637042373254d92e829
KGBT <nik.ivan98@mail.ru>
2024-04-09 19:25:31 +0500

    Переработал базу данных. Создал Status. Добавил Status в UserTask. Переработал некоторые поля, добавив blank=True.

--- task/models.py
+++ task/models.py
@@ -8,16 +8,21 @@ from user.models import User
 class Task(models.Model):
     users = models.ManyToManyField(User, through='UserTask')
     name = models.CharField(max_length=50)
-    description = models.TextField(max_length=1000, null=True)
+    description = models.TextField(max_length=1000, null=True, blank=True)
     date_start = models.DateField()
     date_end = models.DateField()
+    # users = models.ManyToManyField(User, through='UserTask')
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=15)
+    message = models.TextField(max_length=1000, blank=True, null=True)
 
 
 class UserTask(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    status = models.CharField(max_length=15)
-    message = models.TextField(max_length=1000, null=True)
+    status = models.OneToOneField(Status, on_delete=models.CASCADE)
 
 
 class Priority(models.Model):
@@ -27,4 +32,4 @@ class Priority(models.Model):
 
 class File(models.Model):
     task = models.ForeignKey(to=Task, on_delete=models.CASCADE, null=True)
-    file = models.FileField(upload_to='files/', null=True)
+    path = models.FileField(upload_to='files/', null=True, blank=True)
